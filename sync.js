9f83b78176ccea235bfeb019e90ae74f8e82a1db
dbrugne <damien.brugne@gmail.com>
2016-05-11 16:34:33 +0200

    Make env compliant with heroku

--- sync.js
+++ sync.js
@@ -3,10 +3,10 @@ require('./lib/mongo')(process.env.MONGODB_URI || 'mongodb://localhost/test');
 const syncTask = require('./lib/sync/index');
 
 syncTask({
-  host: process.env.HOST,
-  port: process.env.PORT || 22,
-  username: process.env.USERNAME,
-  password: process.env.PASSWORD,
-  dir: process.env.DIR,
-  ignore: process.env.IGNORE,
+  host: process.env.SFTP_HOST,
+  port: process.env.SFTP_PORT || 22,
+  username: process.env.SFTP_USERNAME,
+  password: process.env.SFTP_PASSWORD,
+  dir: process.env.SFTP_DIR,
+  ignore: process.env.SFTP_IGNORE,
 });
