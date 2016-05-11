2bfcd9be47c0f1cb536d151eb7b2cb9017adcc74
dbrugne <damien.brugne@gmail.com>
2016-05-11 14:03:04 +0200

    Reduce syncTask code env deps

--- sync.js
+++ sync.js
@@ -2,4 +2,11 @@ require('dotenv').config();
 require('./lib/mongoConnect')(process.env.MONGO_DB || 'mongodb://localhost/test');
 const syncTask = require('./lib/syncTask');
 
-syncTask();
+syncTask({
+  host: process.env.HOST,
+  port: process.env.PORT || 22,
+  username: process.env.USERNAME,
+  password: process.env.PASSWORD,
+  dir: process.env.DIR,
+  ignore: process.env.IGNORE,
+});
