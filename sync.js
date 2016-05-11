963dfb9233d8b71ad05dbf6099faeb4e02823b47
dbrugne <damien.brugne@gmail.com>
2016-05-11 18:10:22 +0200

    Heroku fix

--- sync.js
+++ sync.js
@@ -1,4 +1,7 @@
-require('dotenv').config();
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
+
 require('./lib/mongo')(process.env.MONGODB_URI || 'mongodb://localhost/test');
 const syncTask = require('./lib/sync/index');
 
