9e84062461bf150e19514b0a1ed28aaa31e16f46
dbrugne <damien.brugne@gmail.com>
2016-05-16 13:41:18 +0200

    Introduce a TMDB cache model

---
+++ test/models-cache.js
@@ -0,0 +1,76 @@
+const should = require('chai').should();
+
+const util = require('../lib/util/tests');
+const Cache = require('../lib/models/cache');
+
+describe('models::cache', () => {
+  before(util.before);
+  beforeEach(util.beforeEach);
+  after(util.after);
+
+  it('is object and has expected functions', () => {
+    Cache.should.be.an('function').and.property('schema');
+    Cache.generateKey.should.be.a('function');
+    Cache.getKey.should.be.a('function');
+    Cache.setKey.should.be.a('function');
+    Cache.purge.should.be.a('function');
+  });
+
+  it('generateKey', () => {
+    Cache.generateKey('searchMethod', {
+      query: 'query',
+      language: 'fr',
+    }).should.equal('searchMethod-{"query":"query","language":"fr"}');
+    Cache.generateKey('searchMethod', 'invalid string').should.match(/^searchMethod-[0-9]+$/);
+  });
+
+  const method = 'searchMethod';
+  const query = {
+    query: 'Title 1',
+    language: 'fr',
+  };
+  const result = {
+    count: 5,
+    results: [{
+      title: 'Title 1',
+      year: 2015,
+      excess: ['FR', 'VOST'],
+    }, {
+      title: 'Title 2',
+      year: 2014,
+      excess: 'MULTI',
+    }],
+  };
+
+  it('setKey', (done) => {
+    Cache.setKey(method, query, result)
+      .then(() => {
+        const key = Cache.generateKey(method, query);
+        Cache.findOne({ key }).exec()
+          .then((doc) => {
+            should.exist(doc);
+            doc.should.be.an('object');
+            doc.should.property('_id').and.be.an('object');
+            doc.should.property('key', key);
+            doc.result.should.deep.equal(result);
+            done();
+          })
+          .catch(err => done(err));
+      });
+  });
+
+  it('getKey', (done) => {
+    Cache.setKey(method, query, result)
+      .then(() => {
+        Cache.getKey(method, query)
+          .then((keyResult) => {
+            should.exist(keyResult);
+            keyResult.should.be.an('object');
+            keyResult.should.deep.equal(result);
+            done();
+          })
+          .catch(err => done(err));
+      })
+      .catch(err => done(err));
+  });
+});
