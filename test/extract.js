1a3406f2d68c8625e7f3aac8999d5c63ac47d168
dbrugne <damien.brugne@gmail.com>
2016-05-17 11:17:44 +0200

    Factorize test fixtures

--- test/extract.js
+++ test/extract.js
@@ -1,16 +1,11 @@
 const should = require('chai').should();
 
 const util = require('../lib/util/tests');
+const fixtures = require('./fixtures.json');
 const opts = {
-  api: util.tmdbMockup({
-    movie: { total_results: 1, results: [{ title: 'Mune Le Gardien De La Lune', year: 2014 }] },
-    tv: { total_results: 1, results: [{ title: 'Mad Men', season: 7, episode: 2 }] },
-    multi: { total_results: 1, results: [{ title: 'Title 1' }] },
-    two: { total_results: 2, results: [{ title: 'Title 2' }, { title: 'Title 1' }] },
-    empty: { total_results: 0 },
-    known: { total_results: 1, results: [{ title: 'Title 1', media_type: 'movie' }] },
-  }),
+  api: util.tmdbMockup(fixtures.tmdb),
 };
+
 const extract = require('../lib/decorate/extract');
 
 describe('decorate::extract', () => {
@@ -26,116 +21,74 @@ describe('decorate::extract', () => {
     promise.should.be.a('promise');
   });
   it('api required', () => extract('/file.mkv').should.be.rejected);
-
   it('unknown', (done) => {
-    extract('/file.mkv', opts)
+    extract(fixtures.tmdb.unknown.file, opts)
       .then((r) => {
-        should.exist(r);
-        r.should.deep.equal({ title: 'Title 1' });
+        should.not.exist(r);
         done();
       })
-      .catch(done);
+      .catch(err => done(new Error(err)));
   });
   it('movie', (done) => {
-    extract('/Mune.Le.Gardien.De.La.Lune.2014.FRENCH.1080p.BluRay.x264-PiNKPANTERS.mkv', opts)
+    extract(fixtures.tmdb.alien.file, opts)
       .then((r) => {
         should.exist(r);
-        r.should.deep.equal({
-          title: 'Mune Le Gardien De La Lune',
-          category: 'movie',
-          year: 2014,
-        });
+        r.should.deep.equal(fixtures.tmdb.alien.result.results[0]);
+        r.should.property('category', 'movie');
         done();
       })
-      .catch(done);
+      .catch(err => done(new Error(err)));
   });
   it('tv', (done) => {
-    extract('/Mad.Men.S07E02.VOSTFR.720p.WEB-DL.DD5.1.H.264-FANATiK.mkv', opts)
+    extract(fixtures.tmdb['mad men'].file, opts)
       .then((r) => {
         should.exist(r);
-        r.should.deep.equal({
-          title: 'Mad Men',
-          category: 'tv',
-          season: 7,
-          episode: 2,
-        });
+        r.should.deep.equal(fixtures.tmdb['mad men'].result.results[0]);
+        r.should.property('category', 'tv');
         done();
       })
       .catch(done);
   });
   it('multi', (done) => {
-    extract('/Title.1.FASTSUB.VOSTFR.1080p.HDTV.x264.AAC-GOBO2S.mkv', opts)
+    extract(fixtures.tmdb.jfk.file, opts)
       .then((r) => {
         should.exist(r);
-        r.should.deep.equal({ title: 'Title 1' });
+        r.should.deep.equal(fixtures.tmdb.jfk.result.results[0]);
+        r.should.property('category', 'movie');
         done();
       })
-      .catch(done);
-  });
-  it('consider only 1st result', (done) => {
-    extract('/two.mkv', opts)
-      .then((r) => {
-        should.exist(r);
-        r.should.be.an('object')
-          .and.property('title', 'Title 2');
-        done();
-      })
-      .catch(done);
-  });
-  it('no result from API', (done) => {
-    extract('/empty.mkv', opts)
-      .then((r) => {
-        should.not.exist(r);
-        done();
-      })
-      .catch(done);
+      .catch(err => done(new Error(err)));
   });
   it('get from cache', (done) => {
-    const path = '/Mune.Le.Gardien.De.La.Lune.2014.FRENCH.1080p.BluRay.x264-PiNKPANTERS.mkv';
-    extract(path, opts)
-      .then(noCache => {
-        should.exist(noCache);
-        noCache.should.deep.equal({
-          title: 'Mune Le Gardien De La Lune',
-          category: 'movie',
-          year: 2014,
-        });
+    // 1st call
+    extract(fixtures.tmdb.jfk.file, opts)
+      .then(first => {
+        should.exist(first);
+        first.should.deep.equal(fixtures.tmdb.jfk.result.results[0]);
 
-        extract(path, opts)
-          .then((withCache) => {
-            should.exist(withCache);
-            withCache.should.deep.equal({
-              title: 'Mune Le Gardien De La Lune',
-              category: 'movie',
-              year: 2014,
-              from_cache: true,
-            });
+        // 2nd call
+        extract(fixtures.tmdb.jfk.file, opts)
+          .then(second => {
+            should.exist(second);
+            const fromCache = Object.assign(
+              { from_cache: true },
+              fixtures.tmdb.jfk.result.results[0]
+            );
+            second.should.deep.equal(fromCache);
             done();
           })
-          .catch(done);
+          .catch(err => done(new Error(err)));
       })
-      .catch(done);
+      .catch(err => done(new Error(err)));
   });
-  describe('category', () => {
-    it('series/', (done) => {
-      extract('/series/unknown.mkv', opts)
-        .then((r) => {
-          should.exist(r);
-          r.should.be.an('object')
-            .and.property('category', 'tv');
-          done();
-        })
-        .catch(done);
-    });
-    it('multi search', (done) => {
-      extract('/known.mkv', opts)
-        .then((r) => {
-          should.exist(r);
-          r.should.be.an('object')
-            .and.property('category', 'movie');
-          done();
-        })
-        .catch(done);
-    });
+  it('force category for series/', (done) => {
+    extract(`/series/${fixtures.tmdb.jfk.file}`, opts)
+      .then((r) => {
+        should.exist(r);
+        r.should.deep.equal(fixtures.tmdb.jfk.result.results[0]);
+        r.should.property('category', 'tv');
+        done();
+      })
+      .catch(err => done(new Error(err)));
   });
 });
