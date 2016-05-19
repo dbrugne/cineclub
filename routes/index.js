89568cc113f8bc8ac805c30132960d8cdcfce9ee
dbrugne <damien.brugne@gmail.com>
2016-05-19 20:01:46 +0200

    Fix HTTP server

--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,30 @@
 const express = require('express');
 const router = express.Router();
 
-const generateDailyEmail = require('../lib/push/emailGenerator');
+const Media = require('../lib/models/media');
+const decorate = require('../lib/decorate/index');
+const render = require('../lib/push/render');
+const tmdbApi = require('moviedb');
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  generateDailyEmail({
-    period: 1,
+  const opts = {
     baseUrl: process.env.BASE_URL,
     replyTo: process.env.MAIL_REPLY_TO,
     premailer: false,
-  }).then((html) => {
-    res.send(html);
-  }).catch(err => {
-    next(err);
-  });
+    api: tmdbApi(process.env.TMBDP_API_KEY),
+  };
+  const period = 1; // day
+  let added;
+  let removed;
+  Media.retrieveAdded(period)
+    .then(docs => decorate(docs, opts))
+    .then(docs => { added = docs; })
+    .then(() => Media.retrieveRemoved(period))
+    .then(docs => { removed = docs; })
+    .then(() => render(added, removed, opts))
+    .then(html => res.end(html))
+    .catch(err => next(err));
 });
 
 module.exports = router;
