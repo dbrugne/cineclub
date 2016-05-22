f7a74f58c57af719b66be37303f5fa8743064f20
dbrugne <damien.brugne@gmail.com>
2016-05-22 17:56:03 +0200

    Fix 404 handling on media/:id route

--- routes/media.js
+++ routes/media.js
@@ -8,24 +8,28 @@ router.get('/:id', (req, res, next) => {
     return next();
   }
 
-  // @todo : test ObjectId validity
-  return Media.findOne({ _id: req.params.id }).exec()
-    .then(doc => {
-      if (!doc) {
-        next();
-        return Promise.resolve();
-      }
-      return doc;
-    })
-    .then(doc => res.json({
+  const id = req.params.id;
+  if (!/^[a-fA-F0-9]{24}$/.test(id)) {
+    return next(new Error('invalid ObjectId'));
+  }
+
+  return Media.findOne({ _id: id }).exec((err, doc) => {
+    if (err) {
+      return next(err);
+    }
+    if (!doc) {
+      return next();
+    }
+
+    return res.json({
       _id: doc.id,
       path: doc.path,
       created: doc.created,
       removed: doc.removed,
       file: doc.file,
       info: doc.info,
-    }))
-    .catch(err => next(err));
+    });
+  });
 });
 
 module.exports = router;
