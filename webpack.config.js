2f590d98f9680c40c17744b2a044cdfc679cd1e7
dbrugne <damien.brugne@gmail.com>
2016-05-26 18:42:11 +0200

    Add webpack/babel/react

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const webpack = require('webpack');
+
+module.exports = {
+  entry: {
+    jsx: './lib/client/index.js',
+    vendor: [
+      'react',
+      'react-dom',
+      'react-redux',
+      'react-router',
+      'react-router-redux',
+      'redux',
+      'whatwg-fetch',
+    ],
+  },
+  output: {
+    path: './public/bundles',
+    publicPath: '/bundles/',
+    filename: 'bundle.js',
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx'],
+  },
+  devServer: {
+    contentBase: './public/bundles/',
+  },
+  module: {
+    loaders: [{
+      test: /\.jsx?$/,
+      exclude: /node_modules/,
+      loader: 'babel',
+    }],
+  },
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin('vendor', 'vendor.js'),
+    new webpack.DefinePlugin({
+      'process.env': { NODE_ENV: JSON.stringify(process.env.NODE_ENV || 'development') },
+    }),
+  ],
+};
