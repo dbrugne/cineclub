be00093336930a78dd1eeecc6a9c52bed4474718
dbrugne <damien.brugne@gmail.com>
2016-05-30 17:28:53 +0200

    Introduce an object flattener utility

---
+++ test/util/flatten.js
@@ -0,0 +1,54 @@
+const chai = require('chai');
+chai.use(require('chai-properties'));
+const should = chai.should();
+
+const flatten = require('../../lib/util/flatten');
+
+describe('util/flatten', () => {
+  it('is function', () => {
+    flatten.should.be.a('function');
+  });
+  it('empty', () => {
+    const r = flatten({});
+    r.should.be.an('object');
+    Object.keys(r).should.have.lengthOf(0);
+  });
+  it('simple (one)', () => {
+    const r = flatten({
+      key1: 'value1',
+    });
+    r.should.be.an('object').and.have.properties({
+      key1: 'value1',
+    });
+  });
+  it('simple (n)', () => {
+    const r = flatten({
+      key1: 'value1',
+      key2: 'value2',
+    });
+    r.should.be.an('object').and.have.properties({
+      key1: 'value1',
+      key2: 'value2',
+    });
+  });
+  it('flatten', () => {
+    const r = flatten({
+      key1: 'value1',
+      deep: {
+        key2: 'value2',
+        key3: 'value3',
+      },
+      verydeep: {
+        deeper: {
+          key4: 'value4',
+        },
+      },
+    });
+    r.should.be.an('object').and.have.properties({
+      key1: 'value1',
+      'deep[key2]': 'value2',
+      'deep[key3]': 'value3',
+      'verydeep[deeper][key4]': 'value4',
+    });
+  });
+});
