1085c63a739415c17c4da9f705936cd59a10537d
dbrugne <damien.brugne@gmail.com>
2016-06-03 00:38:14 +0200

    Working react media page

---
+++ lib/client/pages/media.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import Card from '../components/medias/card';
+
+class Media extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: true,
+      data: null,
+    };
+  }
+  componentDidMount() {
+    this.fetchData();
+  }
+  fetchData() {
+    fetch(`/api/medias/${this.props.params.mediaId}`)
+      .then(response => response.json())
+      .then(data => {
+        this.setState({
+          loading: false,
+          data: data.data,
+        });
+      });
+  }
+  render() {
+    if (this.state.loading === true) {
+      return <div>Loading...</div>;
+    }
+
+    return <Card data={this.state.data} mode="large" />;
+  }
+}
+
+Media.propTypes = {
+  params: React.PropTypes.object,
+};
+
+export default Media;
