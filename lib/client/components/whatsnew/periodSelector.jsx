e8dfd6f9da4b987f8d3abbb252bdc8dc5b790a6a
dbrugne <damien.brugne@gmail.com>
2016-06-03 00:37:10 +0200

    Implement react-router and factorize whatsnew page

--- lib/client/components/whatsnew/periodSelector.jsx
+++ lib/client/components/whatsnew/periodSelector.jsx
@@ -1,34 +1,31 @@
 import React from 'react';
 
-class PeriodSelector extends React.Component {
-  render() {
-    const from = 1;
-    const to = 10;
+const PeriodSelector = props => {
+  const from = 1;
+  const to = 10;
 
-    const options = [];
-    for (let i = from; i <= to; i++) {
-      options.push((
-        <option key={i} value={i}>{i} day(s)</option>
-      ));
-    }
-
-    return (
-      <form className="form-inline pt20">
-        <div className="form-group">
-          <label className="mr10" htmlFor="period">Period</label>
-          <select
-            className="form-control"
-            id="period"
-            defaultValue={this.props.defaultValue}
-            onChange={this.props.update}
-          >
-            {options}
-          </select>
-        </div>
-      </form>
-    );
+  const options = [];
+  for (let i = from; i <= to; i++) {
+    options.push((
+      <option key={i} value={i}>{i} day(s)</option>
+    ));
   }
-}
+
+  return (
+    <form className="form-inline pt20">
+      <div className="form-group">
+        <label className="mr10" htmlFor="period">Period</label>
+        <select
+          className="form-control"
+          id="period"
+          defaultValue={props.defaultValue}
+          onChange={props.update}
+        >
+          {options}
+        </select>
+      </div>
+    </form>);
+};
 
 PeriodSelector.propTypes = {
   defaultValue: React.PropTypes.number,
