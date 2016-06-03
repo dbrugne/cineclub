43709e698f153ce811077fba5b44ae7856d0ba8b
dbrugne <damien.brugne@gmail.com>
2016-06-03 17:40:12 +0200

    Implement redux in media page

--- lib/client/media/Media.jsx
+++ lib/client/media/Media.jsx
@@ -1,38 +1,36 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { fetchMedia } from './MediaActions';
 import Card from '../components/Card';
 
 class Media extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      loading: true,
-      data: null,
-    };
-  }
   componentDidMount() {
-    this.fetchData();
-  }
-  fetchData() {
-    fetch(`/api/medias/${this.props.params.mediaId}`)
-      .then(response => response.json())
-      .then(data => {
-        this.setState({
-          loading: false,
-          data: data.data,
-        });
-      });
+    this.props.dispatch(fetchMedia(this.props.params.mediaId));
   }
   render() {
-    if (this.state.loading === true) {
+    if (this.props.isFetching === true) {
       return <div>Loading...</div>;
+    } else if (!this.props.data) {
+      return null;
     }
 
-    return <Card data={this.state.data} mode="large" />;
+    return <Card data={this.props.data} mode="large" />;
   }
 }
 
 Media.propTypes = {
+  dispatch: React.PropTypes.func,
   params: React.PropTypes.object,
+  data: React.PropTypes.object,
+  isFetching: React.PropTypes.bool,
 };
 
-export default Media;
+function mapStateToProps(state) {
+  return {
+    error: state.media.error,
+    isFetching: state.media.isFetching,
+    data: state.media.data,
+  };
+}
+
+export default connect(mapStateToProps)(Media);
