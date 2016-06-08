ed0577baa340631255226a5e9181c8628c88c32d
dbrugne <damien.brugne@gmail.com>
2016-06-08 18:21:08 +0200

    Minor

--- push.js
+++ push.js
@@ -3,7 +3,7 @@ if (process.env.NODE_ENV !== 'production') {
 }
 
 require('./lib/util/mongo')(process.env.MONGODB_URI || 'mongodb://localhost/test');
-const pushTask = require('./lib/push/index');
+const pushTask = require('./lib/push/task');
 const exit = require('exit');
 
 const tmdb = require('./lib/util/tmdb');
