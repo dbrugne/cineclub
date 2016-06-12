b04b8efc735c987bc3f93c6bf36af0a1e8b10e91
dbrugne <damien.brugne@gmail.com>
2016-06-12 18:24:06 +0200

    Add Pagination component

--- lib/client/medias/Medias.jsx
+++ lib/client/medias/Medias.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { changeCategory, changePage, fetchMedias } from './MediasActions';
 import { Form, FormGroup, ControlLabel, FormControl } from 'react-bootstrap';
+import Pagination from '../components/Pagination';
 import Card from '../components/Card';
 
 class Medias extends React.Component {
@@ -23,50 +24,6 @@ class Medias extends React.Component {
         nextProps.page
       ));
     }
-  }
-  pagination() {
-    if (this.props.pages === 0) {
-      return null;
-    }
-
-    const firstActive = (this.props.page !== 1)
-      ? ''
-      : 'disabled';
-    const lastActive = (this.props.page !== this.props.pages)
-      ? ''
-      : 'disabled';
-
-    const change = (e, i) => {
-      e.preventDefault();
-      this.props.dispatch(changePage(i));
-    };
-
-    const pageLinks = [];
-    for (let i = 1; i <= this.props.pages; i++) {
-      pageLinks.push((
-        <li key={i} className={this.props.page === i ? 'active' : ''}>
-          <a href="#" onClick={e => change(e, i)}>{i}</a>
-        </li>
-      ));
-    }
-
-    return (
-      <nav>
-        <ul className="pagination">
-          <li className={firstActive}>
-            <a href="#" onClick={e => change(e, 1)}>
-              <span>&laquo;</span>
-            </a>
-          </li>
-          {pageLinks}
-          <li className={lastActive}>
-            <a href="#" onClick={e => change(e, this.props.pages)}>
-              <span>&raquo;</span>
-            </a>
-          </li>
-        </ul>
-      </nav>
-    );
   }
   render() {
     if (this.props.isFetching === true) {
@@ -93,7 +50,6 @@ class Medias extends React.Component {
       e.preventDefault();
       this.props.dispatch(changeCategory(e.target.value));
     };
-    const pagination = this.pagination();
 
     return (
       <div>
@@ -119,13 +75,21 @@ class Medias extends React.Component {
         <div className={`p15 ${headerCss}`}>
           {headerContent}
         </div>
-        {pagination}
+        <Pagination
+          pages={this.props.pages}
+          page={this.props.page}
+          onPageChange={page => this.props.dispatch(changePage(page))}
+        />
         <div>
           {this.props.items.map(
             i => <Card data={i} key={i.id} mode="small">{i.title}</Card>
           )}
         </div>
-        {pagination}
+        <Pagination
+          pages={this.props.pages}
+          page={this.props.page}
+          onPageChange={page => this.props.dispatch(changePage(page))}
+        />
       </div>
     );
   }
