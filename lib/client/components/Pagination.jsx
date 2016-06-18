bf2153ff6a2e1f12e27934431d7aa41be3ecb415
dbrugne <damien.brugne@gmail.com>
2016-06-18 18:15:57 +0200

    Minor

--- lib/client/components/Pagination.jsx
+++ lib/client/components/Pagination.jsx
@@ -1,20 +1,26 @@
 import React from 'react';
 import { Pagination } from 'react-bootstrap';
 
-const AdvancedPagination = props => (
-  <Pagination
-    prev
-    next
-    first
-    last
-    ellipsis
-    boundaryLinks
-    items={props.pages}
-    maxButtons={5}
-    activePage={props.page}
-    onSelect={props.changePage}
-  />
-);
+const AdvancedPagination = props => {
+  if (props.pages < 1) {
+    return null;
+  }
+
+  return (
+    <Pagination
+      prev
+      next
+      first
+      last
+      ellipsis
+      boundaryLinks
+      items={props.pages}
+      maxButtons={5}
+      activePage={props.page}
+      onSelect={props.changePage}
+    />
+  );
+};
 
 AdvancedPagination.propTypes = {
   pages: React.PropTypes.number,
